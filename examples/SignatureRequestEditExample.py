002da331388f3a7f36fe418c5bf75439351bcf33
Andrzej Danilowski <adanilowski@dropbox.com>
2026-05-12 15:29:11 +0200

    Release 1.11.0

--- examples/SignatureRequestEditExample.py
+++ examples/SignatureRequestEditExample.py
@@ -51,14 +51,12 @@ with ApiClient(configuration) as api_client:
         files=[
             open("./example_signature_request.pdf", "rb").read(),
         ],
-        metadata=json.loads(
-            """
+        metadata=json.loads("""
             {
                 "custom_id": 1234,
                 "custom_text": "NDA #9"
             }
-        """
-        ),
+        """),
         field_options=field_options,
         signing_options=signing_options,
         signers=signers,
--- examples/SignatureRequestSendExample.py
+++ examples/SignatureRequestSendExample.py
@@ -20,6 +20,7 @@ with ApiClient(configuration) as api_client:
         phone=False,
         type=True,
         upload=True,
+        force_advanced_signature_details=False,
     )
 
     signers_1 = models.SubSignatureRequestSigner(
@@ -51,14 +52,12 @@ with ApiClient(configuration) as api_client:
         files=[
             open("./example_signature_request.pdf", "rb").read(),
         ],
-        metadata=json.loads(
-            """
+        metadata=json.loads("""
             {
                 "custom_id": 1234,
                 "custom_text": "NDA #9"
             }
-        """
-        ),
+        """),
         field_options=field_options,
         signing_options=signing_options,
         signers=signers,
